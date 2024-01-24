4e59be14a9248ca75a2d9347e2e88e47398744ee
jjggu97 <jjggu@naver.com>
2024-01-24 16:47:35 +0900

    1/24

--- rock_scissor_paper_simulation.py
+++ rock_scissor_paper_simulation.py
@@ -1,22 +1,39 @@
 import random
+import matplotlib.pyplot as plt
 
-# select player`s choice
-my_choice = input("Select rock, paper, scissor :")
-print("Your Choice is..." + my_choice)
+def play_game():
+    choices = ["rock", "paper", "scissor"]
+    my_choice = random.choice(choices)
+    computer_choice = random.choice(choices)
 
-choices = ["rock","paper","scissor"]
+    if my_choice == computer_choice:
+        return 'draw'
+    elif (my_choice == "rock" and computer_choice == "scissor") or \
+         (my_choice == "paper" and computer_choice == "rock") or \
+         (my_choice == "scissor" and computer_choice == "paper"):
+        return 'win'
+    else:
+        return 'lose'
 
-# select computer`s choice
-computer_choice = random.choice(choices)
-print("Computer`s Choice is..."+computer_choice)
+def visualize_win_probability(num_simulations):
+    wins = 0
+    results = {'win': 0, 'draw': 0, 'lose': 0}
 
-# rock, paper, scissor!
-if my_choice == computer_choice:
-    print("Draw!")
-elif(my_choice == "rock" and computer_choice == "scissor") or (my_choice == "paper" and computer_choice == "rock") or (my_choice == "scissor" and computer_choice == "paper"):
-    print("You Win!")
-else:
-    print("You lose...")
+    for _ in range(num_simulations):
+        result = play_game()
+        results[result] += 1
+        if result == 'win':
+            wins += 1
 
+    win_probability = wins / num_simulations
 
-# previous code
+    # visualization
+    labels = list(results.keys())
+    values = list(results.values())
+
+    plt.bar(labels, values)
+    plt.title(f"Win Probability after {num_simulations} Simulations: {win_probability:.2%}")
+    plt.show()
+
+# adjust number of simulations
+visualize_win_probability(num_simulations=1000)
